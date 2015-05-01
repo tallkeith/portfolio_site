e8fccf7a9b5337b5b3fa7e4172f130da72a11902
tallkeith <tallestkeith@gmail.com>
2015-05-01 14:19:12 -0400

    Add validation to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,8 +24,12 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    respond_with(@post)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def update
