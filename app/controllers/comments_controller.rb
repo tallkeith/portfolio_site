41ea9a103acdb66ac5a62bfea2edc05202973617
tallkeith <tallestkeith@gmail.com>
2015-04-30 23:35:35 -0400

    still working on the blog posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,12 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
     redirect_to @post
   end
+
+  def destroy
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comment.find(params[:id])
+  	@comment.destroy
+
+  	redirect_to post_path(@post)
+  end 
 end
